50b3ae7b39ffebde478ea3659fe9370d6dd30691
Suyash <suyash5802@gmail.com>
2023-12-21 18:04:35 +0530

    first commit

---
+++ src/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header/header'
+import Footer from './components/Footer/Footer'
+import {Outlet} from 'react-router-dom'
+
+
+export default function Layout() {
+  return (
+    <div>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+
+    </div>
+  )
+}
